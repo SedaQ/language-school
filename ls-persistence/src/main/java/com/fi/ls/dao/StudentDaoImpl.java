b04ff736edc7373ed9cf453b3139d096ca38ead6
Healadin145 <matus.guoth@gmail.com>
2016-10-23 18:47:46 +0200

    created entity Student + DAO + DAOImpl

---
+++ ls-persistence/src/main/java/com/fi/ls/dao/StudentDaoImpl.java
@@ -0,0 +1,46 @@
+package com.fi.ls.dao;
+
+import com.fi.ls.entity.Student;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author Matúš
+ */
+@Repository
+@Transactional
+public class StudentDaoImpl implements StudentDao {
+    
+    @PersistenceContext
+	private EntityManager em;
+
+    @Override
+    public void create(Student s) {
+        em.persist(s);
+    }
+
+    @Override
+    public Student findById(Long id) {
+        return em.find(Student.class, id);
+    }
+
+    @Override
+    public Student update(Student s) {
+        return em.merge(s);
+    }
+
+    @Override
+    public void remove(Student s) {
+        em.remove(findById(s.getId()));
+    }
+
+    @Override
+    public List<Student> findAll() {
+        return em.createNamedQuery("Student.findAll", Student.class).getResultList();
+    }
+    
+}
