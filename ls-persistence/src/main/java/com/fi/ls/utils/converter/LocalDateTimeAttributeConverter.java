34c369b7f57b8bbfa648a389a9e53b5e29e1d07a
SedaQ <pavelseda@email.cz>
2016-10-24 16:24:09 +0200

    LocalDateTime converter added.

---
+++ ls-persistence/src/main/java/com/fi/ls/utils/converter/LocalDateTimeAttributeConverter.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.fi.ls.utils.converter;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+
+/**
+ * @author Pavel Šeda (441048)
+ *
+ */
+@Converter(autoApply = true)
+public class LocalDateTimeAttributeConverter implements AttributeConverter<LocalDateTime, Timestamp> {
+
+	@Override
+	public Timestamp convertToDatabaseColumn(LocalDateTime locDateTime) {
+		return (locDateTime == null ? null : Timestamp.valueOf(locDateTime));
+	}
+
+	@Override
+	public LocalDateTime convertToEntityAttribute(Timestamp sqlTimestamp) {
+		return (sqlTimestamp == null ? null : sqlTimestamp.toLocalDateTime());
+	}
+}
--- ls-persistence/src/test/java/com/fi/ls/dao/LanguageDaoTest.java
+++ ls-persistence/src/test/java/com/fi/ls/dao/LanguageDaoTest.java
@@ -33,76 +33,117 @@ public class LanguageDaoTest extends AbstractTestNGSpringContextTests {
 	@Autowired
 	private LanguageDao languageDao;
 
-	private Lecturer lect1;
-	private Lecturer lect2;
-	private Language lan1;
-	private Language lan2;
+	Lecturer lect1;
+	Language lan1;
 
 	@BeforeMethod
 	public void init() {
-		lect1 = new Lecturer();
-		lect1.setFirstName("first name lect1");
-		lect1.setSurname("surname lect1");
-
-		lect2 = new Lecturer();
-		lect2.setFirstName("first name lect2");
-		lect2.setSurname("surname lect2");
-
 		lan1 = new Language();
 		lan1.setLanguage("ENG");
 		lan1.setCefrLever(ProficiencyLevel.A1);
 
-		lan2 = new Language();
-		lan2.setLanguage("CZE");
-		lan2.setCefrLever(ProficiencyLevel.A2);
-
+		lect1 = new Lecturer();
+		lect1.setFirstName("first name lect1");
+		lect1.setSurname("surname lect1");
 		lect1.addLanguage(lan1);
-		lect2.addLanguage(lan2);
 
 		em.persist(lect1);
-		em.persist(lect2);
-
-		languageDao.create(lan1);
-		languageDao.create(lan2);
 	}
 
 	@Test
 	public void testCreate() {
-		Assert.assertNotNull(languageDao.findById(lan1.getId()));
+		languageDao.create(lan1);
+		Assert.assertNotNull(em.find(Language.class, lan1.getId()));
 	}
 
-	@Test
-	public void testFindAll() {
-		Assert.assertEquals(languageDao.findAll().size(), 2);
+	@Test(expectedExceptions = org.springframework.dao.InvalidDataAccessApiUsageException.class)
+	public void testCreateNull() {
+		languageDao.create(null);
+	}
+
+	@Test(expectedExceptions = javax.validation.ConstraintViolationException.class)
+	public void testCreateNullLanguage() {
+		lan1.setLanguage(null);
+		languageDao.create(lan1);
+	}
+
+	@Test(expectedExceptions = javax.validation.ConstraintViolationException.class)
+	public void testCreateNullLecturer() {
+		lan1.setLecturer(null);
+		languageDao.create(lan1);
+	}
+
+	@Test(expectedExceptions = javax.validation.ConstraintViolationException.class)
+	public void testCreateNullCefrLevel() {
+		lan1.setCefrLever(null);
+		languageDao.create(lan1);
 	}
 
 	@Test
 	public void testFindById() {
+		em.persist(lan1);
 		Assert.assertEquals(languageDao.findById(lan1.getId()).getLanguage(), "ENG");
 		Assert.assertNotEquals(languageDao.findById(lan1.getId()).getLanguage(), "Some other language");
 	}
 
+	@Test(expectedExceptions = org.springframework.dao.InvalidDataAccessApiUsageException.class)
+	public void testFindByIdNull() {
+		em.persist(lan1);
+		languageDao.findById(null);
+	}
+
 	@Test
 	public void testUpdate() {
+		em.persist(lan1);
 		lan1.setLanguage("TEST update language");
 		languageDao.update(lan1);
 		Assert.assertEquals(languageDao.findById(lan1.getId()).getLanguage(), "TEST update language");
 		Assert.assertNotEquals(languageDao.findById(lan1.getId()).getLanguage(), "ENG");
 	}
 
+	@Test(expectedExceptions = org.springframework.dao.InvalidDataAccessApiUsageException.class)
+	public void testUpdateNull() {
+		em.persist(lan1);
+		languageDao.update(null);
+	}
+
+	@Test
+	public void testFindAll() {
+		Lecturer lect2 = new Lecturer();
+		lect2.setFirstName("first name lect2");
+		lect2.setSurname("surname lect2");
+
+		Language lan2 = new Language();
+		lan2.setLanguage("CZE");
+		lan2.setCefrLever(ProficiencyLevel.A2);
+		lect2.addLanguage(lan2);
+
+		em.persist(lect1);
+		em.persist(lect2);
+		languageDao.create(lan2);
+		languageDao.create(lan1);
+
+		Assert.assertEquals(languageDao.findAll().size(), 2);
+	}
+
 	@Test
 	public void testRemove() {
+		em.persist(lan1);
 		Assert.assertNotNull(languageDao.findById(lan1.getId()));
 		languageDao.remove(lan1);
 		Assert.assertNull(languageDao.findById(lan1.getId()));
 	}
 
+	@Test(expectedExceptions = NullPointerException.class)
+	public void testRemoveNull() {
+		em.persist(lan1);
+		languageDao.remove(null);
+	}
+
 	@AfterMethod
 	public void clearMemory() {
 		lect1 = null;
-		lect2 = null;
 		lan1 = null;
-		lan2 = null;
 	}
 
 }
