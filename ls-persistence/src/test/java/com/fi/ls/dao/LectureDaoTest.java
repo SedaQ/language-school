2b0592166517018201914fd89463c9a238812ba2
Healadin145 <matus.guoth@gmail.com>
2016-10-25 20:37:21 +0200

    LectureDaoTest

--- ls-persistence/src/test/java/com/fi/ls/dao/LectureDaoTest.java
+++ ls-persistence/src/test/java/com/fi/ls/dao/LectureDaoTest.java
@@ -1,87 +1,126 @@
 package com.fi.ls.dao;
 
-import com.fi.ls.entity.Course;
 import com.fi.ls.entity.Lecture;
-import com.fi.ls.entity.Lecturer;
-import com.fi.ls.entity.Student;
 import java.time.LocalDateTime;
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.PersistenceUnit;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
 import javax.validation.ValidationException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
 import org.testng.Assert;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 /**
  *
- * @author Matúš
+ * @author Matus
  */
 @ContextConfiguration(classes = com.fi.ls.context.PersistenceApplicationContext.class)
+@TestExecutionListeners(TransactionalTestExecutionListener.class)
+@Transactional
 public class LectureDaoTest extends AbstractTestNGSpringContextTests {
     
-    @PersistenceUnit
-    private EntityManagerFactory emf;
+    @PersistenceContext
+    private EntityManager em;
     
-    Lecture l1 = new Lecture();
-    Lecture l2 = new Lecture();
-    Lecture l3 = new Lecture();
-    Student s1 = new Student();
-    Student s2 = new Student();
-    Lecturer l = new Lecturer();
-    Course c = new Course();
+    @Autowired
+    private LectureDao lectureDao;
     
-    @BeforeClass
-    public void beforeClass() {
-        EntityManager em = emf.createEntityManager();
-        em.getTransaction().begin();
-        
-        l1.addCourse(c);
-        l2.addCourse(c);
-        
-        l1.addLecturer(l);
-        l2.addLecturer(l);
-        
-        l1.addStudent(s1);
-        l1.addStudent(s2);
-        l2.addStudent(s1);
-        
-        l1.setDayTime(LocalDateTime.MIN);
-        l2.setDayTime(LocalDateTime.MIN);
-        
-        l1.setTopic("Random talk");
-        l2.setTopic("Random talk");
-        
-        em.persist(l1);
-        em.persist(l2);
-        
-        em.getTransaction().commit();
-        em.close();
+    private Lecture l;
+    
+    @BeforeMethod
+    public void beforeMethod() {
+        l = new Lecture();
+        l.setDayTime(LocalDateTime.MIN);
+        l.setTopic("Something");
     }
-    /**
+    
     @Test(expectedExceptions = ValidationException.class)
-    public void nullIntoNotNullColumn() {
-        EntityManager em = emf.createEntityManager();
-        em.getTransaction().begin();
-        
-        em.persist(l3);
-        
-        //sem by sa to nemalo dostat - len na commit do branchu
-        em.getTransaction().commit();
-        em.close();
-    }*/
-    @Test
-    public void lectureHasStudents() {
-        EntityManager em = emf.createEntityManager();
-        em.getTransaction().begin();
-        
-        Lecture tl = em.find(Lecture.class, l1.getId());
-        Assert.assertEquals(tl.getListOfStudents().size(), 2);
-        
-        em.getTransaction().commit();
-        em.close();
+    public void nullDayTimeNotAllowed () {
+        l.setDayTime(null);
+        lectureDao.create(l);
     }
+    
+    @Test(expectedExceptions = ValidationException.class)
+    public void nullTopicNotAllowed () {
+        l.setTopic(null);
+        lectureDao.create(l);
+    }
+    //odtialto
+    @Test
+    public void testFindById() {
+        lectureDao.create(l);
+        Assert.assertEquals(l, lectureDao.findById(l.getId()));
+    }
+    
+    @Test
+    public void testFindAll() {
+        Lecture l1 = new Lecture();
+        l1.setDayTime(LocalDateTime.MAX);
+        l1.setTopic("Random topic");
+        
+        lectureDao.create(l);
+        lectureDao.create(l1);
+        
+        Assert.assertEquals(lectureDao.findAll().size(), 2);
+        Assert.assertTrue(lectureDao.findAll().contains(l));
+        Assert.assertTrue(lectureDao.findAll().contains(l1));
+    }
+    
+    @Test
+    public void testCreateAndRemove() {
+        lectureDao.create(l);
+        Assert.assertEquals(lectureDao.findAll().size(), 1);
+        lectureDao.remove(l);
+        Assert.assertEquals(lectureDao.findAll().size(), 0);
+    }
+    
+    @Test
+    public void testUpdate() {
+        lectureDao.create(l);
+        l.setTopic("Something else");
+        lectureDao.update(l);
+        Assert.assertEquals(l.getTopic(), lectureDao.findById(l.getId()).getTopic());
+    }
+    /*
+    @Test
+    public void testFindById() {
+        em.persist(l);
+        Assert.assertEquals(l, lectureDao.findById(l.getId()));
+    }
+    
+    @Test
+    public void testFindAll() {
+        Lecture l1 = new Lecture();
+        l1.setDayTime(LocalDateTime.MAX);
+        l1.setTopic("Random topic");
+        
+        em.persist(l);
+        em.persist(l1);
+        
+        Assert.assertEquals(lectureDao.findAll().size(), 2);
+        Assert.assertTrue(lectureDao.findAll().contains(l));
+        Assert.assertTrue(lectureDao.findAll().contains(l1));
+    }
+    
+    @Test
+    public void testCreateAndRemove() {
+        em.persist(l);
+        Assert.assertEquals(lectureDao.findAll().size(), 1);
+        em.remove(l);
+        Assert.assertEquals(lectureDao.findAll().size(), 0);
+    }
+    
+    @Test
+    public void testUpdate() {
+        em.persist(l);
+        l.setTopic("Something else");
+        em.persist(l);
+        Assert.assertEquals(l.getTopic(), lectureDao.findById(l.getId()).getTopic());
+    }
+    */
 }
