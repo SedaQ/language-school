61045bf87c03046489b9ff447e0564315e99c599
Healadin145 <matus.guoth@gmail.com>
2016-10-30 20:39:48 +0100

    First Name fix

--- ls-persistence/src/test/java/com/fi/ls/entity/StudentTest.java
+++ ls-persistence/src/test/java/com/fi/ls/entity/StudentTest.java
@@ -17,17 +17,17 @@ public class StudentTest {
     @BeforeClass
     public void beforeClass() {
 	s1 = new Student();
-        s1.setName("Jim");
+        s1.setFirstName("Jim");
         s1.setSurname("Carry");
         s1.setBirthNumber("8811229999");
 
 	s2 = new Student();
-        s2.setName("Ozzy");
+        s2.setFirstName("Ozzy");
         s2.setSurname("Osbourne");
         s2.setBirthNumber("8811229999");
         
 	s3 = new Student();
-        s3.setName("Jim");
+        s3.setFirstName("Jim");
         s3.setSurname("Carry");
         s3.setBirthNumber("8811229999");
     }
