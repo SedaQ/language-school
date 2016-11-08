f3d15478593e666777175365e5f4c3da984d4a9a
SedaQ <pavelseda@email.cz>
2016-11-08 06:37:45 +0100

    LSUserRepositoryTest fixed all tests.

--- ls-persistence/src/test/java/com/fi/ls/dao/LSUserRepositoryTest.java
+++ ls-persistence/src/test/java/com/fi/ls/dao/LSUserRepositoryTest.java
@@ -9,9 +9,12 @@ import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
 import org.springframework.transaction.annotation.Transactional;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import com.fi.ls.entity.Course;
 import com.fi.ls.entity.LSUser;
 
 /**
@@ -36,56 +39,56 @@ public class LSUserRepositoryTest extends AbstractTestNGSpringContextTests {
 		user = new LSUser();
 		user.setEmail("test123@email.cz");
 		user.setPasswordHash("testHashtestHashtestHashtestHash7841267871s!@$%");
-		 userDao.deleteAll();
-		// Assert.assertEquals(userDao.count(), 0);
+		userDao.deleteAll();
+		Assert.assertEquals(userDao.count(), 0);
 	}
-//
-//	@Test
-//	public void testCreate() {
-//		userDao.save(user);
-//		Assert.assertNotNull(em.find(Course.class, user.getId()));
-//	}
-//
-//	@Test
-//	public void testRemove() {
-//		LSUser userToRemove = new LSUser();
-//		user.setEmail("testToRemove@email.cz");
-//		user.setPasswordHash("testHashtestHashtestHashtestHasdqewash7841267871s!@$%");
-//		userDao.save(userToRemove);
-//
-//		Assert.assertNotNull(userDao.findOne(userToRemove.getId()));
-//		userDao.delete(userToRemove);
-//		Assert.assertNull(userDao.findOne(userToRemove.getId()));
-//	}
-//
-//	@Test
-//	public void testUpdate() {
-//		userDao.save(user);
-//		user.setEmail("testEmailUpdated@email.cz");
-//		userDao.save(user);
-//		Assert.assertEquals(userDao.findOne(user.getId()).getEmail(), "testEmailUpdated@email.cz");
-//		Assert.assertNotEquals(userDao.findOne(user.getId()).getEmail(), "testFakeEmail@email.cz");
-//	}
-//
-//	@Test
-//	public void testFindById() {
-//		userDao.save(user);
-//		Assert.assertEquals(user.getId(), userDao.findOne(user.getId()));
-//	}
-//
-//	@Test
-//	public void testFindAll() {
-//		userDao.save(user);
-//		LSUser user2 = new LSUser();
-//		user2.setEmail("test2@email.cz");
-//		user2.setPasswordHash("testHashtestHashtestHashtestHash7841267871s!@$%");
-//		userDao.save(user2);
-//		Assert.assertEquals(userDao.findAll().size(), 2);
-//	}
-//
-//	@AfterMethod
-//	public void clearMemory() {
-//		user = null;
-//	}
+
+	@Test
+	public void testCreate() {
+		userDao.save(user);
+		Assert.assertNotNull(em.find(LSUser.class, user.getId()));
+	}
+
+	@Test
+	public void testRemove() {
+		LSUser userToRemove = new LSUser();
+		userToRemove.setEmail("testToRemove@email.cz");
+		userToRemove.setPasswordHash("testHashtestHashtestHashtestHasdqewash7841267871s!@$%");
+		userDao.save(userToRemove);
+
+		Assert.assertNotNull(userDao.findOne(userToRemove.getId()));
+		userDao.delete(userToRemove);
+		Assert.assertNull(userDao.findOne(userToRemove.getId()));
+	}
+
+	@Test
+	public void testUpdate() {
+		userDao.save(user);
+		user.setEmail("testEmailUpdated@email.cz");
+		userDao.save(user);
+		Assert.assertEquals(userDao.findOne(user.getId()).getEmail(), "testEmailUpdated@email.cz");
+		Assert.assertNotEquals(userDao.findOne(user.getId()).getEmail(), "testFakeEmail@email.cz");
+	}
+
+	@Test
+	public void testFindById() {
+		userDao.save(user);
+		Assert.assertEquals(user, userDao.findOne(user.getId()));
+	}
+
+	@Test
+	public void testFindAll() {
+		userDao.save(user);
+		LSUser user2 = new LSUser();
+		user2.setEmail("test2@email.cz");
+		user2.setPasswordHash("testHashtestHashtestHashtestHash7841267871s!@$%");
+		userDao.save(user2);
+		Assert.assertEquals(userDao.findAll().size(), 2);
+	}
+
+	@AfterMethod
+	public void clearMemory() {
+		user = null;
+	}
 
 }
