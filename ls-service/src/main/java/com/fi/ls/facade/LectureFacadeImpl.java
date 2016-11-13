da1f487c7762563a572664bf47bf3c5f7b6009fe
xthobi <nedbalmarek@gmail.com>
2016-11-13 19:02:45 +0100

    xthobi - LectureFacade, LectureService + fix in Lecture

---
+++ ls-service/src/main/java/com/fi/ls/facade/LectureFacadeImpl.java
@@ -0,0 +1,64 @@
+package com.fi.ls.facade;
+
+import com.fi.ls.dto.LectureDTO;
+import com.fi.ls.entity.Lecture;
+import com.fi.ls.mapping.BeanMapping;
+import com.fi.ls.service.LectureService;
+import java.util.List;
+import java.util.Optional;
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author Marek Nedbal (357293)
+ */
+@Service
+@Transactional
+public class LectureFacadeImpl implements LectureFacade {
+    
+    @Inject
+    private LectureService lectureService;
+    
+    @Inject
+    private BeanMapping beanMapping;
+    
+    @Override
+    public void createLecture(LectureDTO lecture) {
+        
+        lectureService.create(beanMapping.mapTo(lecture, Lecture.class).get());
+        
+    }
+
+    @Override
+    public Optional<LectureDTO> getLectureById(Long id) {
+        
+        Optional<Lecture> lecture = Optional.of(lectureService.findById(id));
+        return lecture.isPresent() ? beanMapping.mapTo(lecture.get(), LectureDTO.class) : Optional.empty();
+        
+    }
+
+    @Override
+    public List<LectureDTO> getAllLectures() {
+        
+        return beanMapping.mapTo(lectureService.findAll(), LectureDTO.class);
+        
+    }
+
+    @Override
+    public Optional<LectureDTO> updateLecture(Long id) {
+        
+        Optional<Lecture> lecture = Optional.of(lectureService.update(lectureService.findById(id)));
+        return lecture.isPresent() ? beanMapping.mapTo(lecture.get(), LectureDTO.class) : Optional.empty();
+        
+    }
+
+    @Override
+    public void deleteLecture(Long id) {
+        
+        lectureService.remove(lectureService.findById(id));
+        
+    }
+    
+}
---
+++ ls-service/src/main/java/com/fi/ls/service/LectureServiceImpl.java
@@ -0,0 +1,52 @@
+package com.fi.ls.service;
+
+import com.fi.ls.dao.LectureDao;
+import com.fi.ls.entity.Lecture;
+import java.util.List;
+import javax.inject.Inject;
+
+/**
+ *
+ * @author Marek Nedbal (357293)
+ */
+public class LectureServiceImpl implements LectureService {
+    
+    @Inject
+    private LectureDao lectureDao;
+    
+    @Override
+    public void create(Lecture lecture) {
+        
+        lectureDao.create(lecture);
+        
+    }
+
+    @Override
+    public Lecture findById(Long id) {
+        
+        return lectureDao.findById(id);
+        
+    }
+
+    @Override
+    public Lecture update(Lecture lecture) {
+        
+        return lectureDao.update(lecture);
+        
+    }
+
+    @Override
+    public void remove(Lecture lecture) {
+        
+        lectureDao.remove(lecture);
+        
+    }
+
+    @Override
+    public List<Lecture> findAll() {
+        
+        return lectureDao.findAll();
+        
+    }
+    
+}
