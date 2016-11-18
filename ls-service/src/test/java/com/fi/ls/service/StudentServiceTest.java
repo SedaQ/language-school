bae4601f36cf7337d18362c2be0af64e3abf5275
Healadin145 <matus.guoth@gmail.com>
2016-11-18 19:43:18 +0100

    Test progress (some are not working yet)
    4 functions left to test

--- ls-service/src/test/java/com/fi/ls/service/StudentServiceTest.java
+++ ls-service/src/test/java/com/fi/ls/service/StudentServiceTest.java
@@ -2,16 +2,23 @@ package com.fi.ls.service;
 
 import com.fi.ls.dao.StudentDao;
 import com.fi.ls.entity.Student;
+import com.fi.ls.exceptions.ServiceLayerException;
+import java.util.List;
+import javax.persistence.PersistenceException;
 import org.mockito.Mock;
-import org.springframework.test.context.testng.AbstractTransactionalTestNGSpringContextTests;
+import org.mockito.MockitoAnnotations;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import static org.mockito.Mockito.*;
+import static org.testng.Assert.*;
 
 /**
  *
  * @author Matúš
  */
-public class StudentServiceTest extends AbstractTransactionalTestNGSpringContextTests {
+public class StudentServiceTest {
     
     @Mock
     private StudentDao studentDao;
@@ -21,6 +28,12 @@ public class StudentServiceTest extends AbstractTransactionalTestNGSpringContext
     Student s1;
     Student s2;
     Student s3;
+    
+    @BeforeClass
+    public void beforeClass() {
+        MockitoAnnotations.initMocks(this);
+        studentService = new StudentServiceImpl(studentDao);
+    }
     
     @BeforeMethod
     public void beforeMethod() {
@@ -36,6 +49,10 @@ public class StudentServiceTest extends AbstractTransactionalTestNGSpringContext
         s2.setEmail("enterprise.java2@project.com");
         s3.setEmail("enterprise.java3@project.com");
         
+        s1.setPasswordHash("hashTest123456");
+        s2.setPasswordHash("hashTest456789");
+        s3.setPasswordHash("hashTest987456");
+        
         s1.setFirstName("enterprise");
         s2.setFirstName("enterprise");
         s3.setFirstName("enterprise3");
@@ -43,10 +60,173 @@ public class StudentServiceTest extends AbstractTransactionalTestNGSpringContext
         s1.setSurname("java1");
         s2.setSurname("java");
         s3.setSurname("java");
-        
-        //s1.setPasswordHash(UserPasswordEncryption.createHash("project1"));
-        //s2.setPasswordHash(UserPasswordEncryption.createHash("project2"));
-        //s3.setPasswordHash(UserPasswordEncryption.createHash("project3"));
     }
 
+    @Test
+    public void testCreate(){
+        studentService.create(s1);
+        verify(studentDao, times(1)).create(s1);
+    }
+    
+    @Test(expectedExceptions = {IllegalArgumentException.class})
+    public void testCreateNull(){
+        studentService.create(null);
+        fail("Null has been created.");
+    }
+    
+    @Test(expectedExceptions = {ServiceLayerException.class})
+    public void testCreateServiceException(){
+        doThrow(new PersistenceException("")).when(studentDao).create(any(Student.class));
+        studentService.create(s1);
+        fail("Expected service layer exception in create.");
+    }
+    
+    @Test
+    public void testRemove(){
+        studentService.remove(s1);
+        verify(studentDao, times(1)).remove(s1);
+    }
+    
+    @Test(expectedExceptions = {IllegalArgumentException.class})
+    public void testRemoveNull(){
+        studentService.remove(null);
+        fail("Null has been removed.");
+    }
+    
+    @Test(expectedExceptions = {ServiceLayerException.class})
+    public void testRemoveServiceException(){
+        doThrow(new PersistenceException("")).when(studentDao).remove(any(Student.class));
+        studentService.remove(s1);
+        fail("Expected service layer exception in update.");
+    }
+    
+    @Test
+    public void testUpdate(){
+        s1.setBirthNumber("123");
+        studentService.update(s1);
+        verify(studentDao, times(1)).update(s1);
+    }
+    
+    @Test(expectedExceptions = {IllegalArgumentException.class})
+    public void testNullUpdate(){
+        studentService.update(null);
+        fail("Null has been updated.");
+    }
+    
+    @Test(expectedExceptions = {ServiceLayerException.class})
+    public void testUpdateServiceException(){
+        doThrow(new PersistenceException("")).when(studentDao).update(any(Student.class));
+        s1.setBirthNumber("123");
+        studentService.update(s1);
+        fail("Expected service layer exception in update.");
+    }
+    
+    @Test
+    public void testFindByBirthNumber(){
+        studentService.findByBirthNumber("number");
+        verify(studentDao, times(1)).findByBirthNumber("number");
+    }
+    
+    @Test(expectedExceptions = {IllegalArgumentException.class})
+    public void testFindByBirthNumberNull(){
+        studentService.findByBirthNumber(null);
+        fail("Found by null birth number.");
+    }
+    
+    @Test(expectedExceptions = {ServiceLayerException.class})
+    public void testFindByBirthNumberServiceException(){
+        doThrow(new PersistenceException("")).when(studentDao).findByBirthNumber(any(String.class));
+        studentService.findByBirthNumber("number");
+        fail("Expected service layer exception in find by birth number.");
+    }
+    
+    @Test
+    public void testFindById(){
+        studentService.findById(Long.MIN_VALUE);
+        verify(studentDao, times(1)).findById(Long.MIN_VALUE);
+    }
+    
+    @Test(expectedExceptions = {IllegalArgumentException.class})
+    public void testFindByIdNull(){
+        studentService.findById(null);
+        fail("Found by null id.");
+    }
+            
+    @Test(expectedExceptions = {ServiceLayerException.class})
+    public void testFindByIdServiceException(){
+        doThrow(new PersistenceException("")).when(studentDao).findById(any(Long.class));
+        studentService.findById(Long.MIN_VALUE);
+        fail("Expected service layer exception in find by id.");
+    }
+    
+    @Test
+    public void testFindAllStudents(){
+        studentService.findAllStudents();
+        verify(studentDao, times(1)).findAll();
+    }
+    
+    @Test(expectedExceptions = {ServiceLayerException.class})
+    public void testFindAllStudentsServiceException(){
+        doThrow(new PersistenceException("")).when(studentDao).findAll();
+        studentService.findAllStudents();
+        fail("Expected service layer exception in find by id.");
+    }
+    
+    @Test
+    public void testFindByFirstName(){
+        studentService.create(s1);
+        studentService.create(s2);
+        studentService.create(s3);
+        List<Student> s = studentService.findByFirstName("enterprise");
+        assertEquals(s.size(), 2);
+    }
+    
+    @Test
+    public void testFindByFirstName2(){
+        studentService.findByFirstName("test");
+        verify(studentDao, times(1)).findByFirstName("test");
+    }
+    
+    @Test(expectedExceptions = {IllegalArgumentException.class})
+    public void testFindByFirstNameNull(){
+        studentService.findByFirstName(null);
+        fail("Found by null first name.");
+    }
+    
+    @Test(expectedExceptions = {ServiceLayerException.class})
+    public void testFindByFirstNameServiceException(){
+        doThrow(new PersistenceException("")).when(studentDao).findByFirstName(any(String.class));
+        studentService.findByFirstName("test");
+        fail("Expected service layer exception in find by surname.");
+    }
+    
+    @Test
+    public void testFindBySurname(){
+        studentService.create(s1);
+        studentService.create(s2);
+        studentService.create(s3);
+        List<Student> s = studentService.findBySurname("java");
+        assertEquals(s.size(), 2);
+    }
+    
+    @Test
+    public void testFindBySurname2(){
+        studentService.findBySurname("test");
+        verify(studentDao, times(1)).findBySurname("test");
+    }
+    
+    @Test(expectedExceptions = {IllegalArgumentException.class})
+    public void testFindBySurnameNull(){
+        studentService.findBySurname(null);
+        fail("Found by null first name.");
+    }
+    
+    @Test(expectedExceptions = {ServiceLayerException.class})
+    public void testFindBySurnameServiceException(){
+        doThrow(new PersistenceException("")).when(studentDao).findBySurname(any(String.class));
+        studentService.findBySurname("test");
+        fail("Expected service layer exception in find by surname.");
+    }
+    
+    
 }
