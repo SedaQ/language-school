37f836101f3069f8dba4dd42211a5b538c7d933d
SedaQ <pavelseda@email.cz>
2016-11-20 23:05:12 +0100

    UserPasswordEncryptionTest added.

---
+++ ls-service/src/test/java/com/fi/ls/security/UserPasswordEncryptionTest.java
@@ -0,0 +1,35 @@
+package com.fi.ls.security;
+
+import org.junit.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+/**
+ * @author Pavel Šeda (441048)
+ *
+ */
+public class UserPasswordEncryptionTest {
+
+	private UserPasswordEncryption userPwdEncryption;
+
+	private String password;
+	private String passwordHash;
+
+	@BeforeClass()
+	public void init() {
+		password = "testPassword";
+		userPwdEncryption = new UserPasswordEncryption();
+	}
+
+	@Test
+	public void testCreateHash() {
+		passwordHash = userPwdEncryption.createHash(password);
+		Assert.assertNotEquals(password, passwordHash);
+	}
+
+	@Test
+	public void testValidatePassword() {
+		Assert.assertTrue(userPwdEncryption.validatePassword(password, passwordHash));
+	}
+
+}
