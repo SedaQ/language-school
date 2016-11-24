a3a37575a96706213a74fa4b5ceaeb3f7ea8f180
xthobi <nedbalmarek@gmail.com>
2016-11-24 21:49:27 +0100

    xthobi - LectureFacadeImpl fix

--- ls-service/src/main/java/com/fi/ls/facade/LectureFacadeImpl.java
+++ ls-service/src/main/java/com/fi/ls/facade/LectureFacadeImpl.java
@@ -3,6 +3,7 @@ package com.fi.ls.facade;
 import com.fi.ls.dto.lecture.LectureCreateDTO;
 import com.fi.ls.dto.lecture.LectureDTO;
 import com.fi.ls.entity.Lecture;
+import com.fi.ls.exceptions.ServiceLayerException;
 import com.fi.ls.mapping.BeanMapping;
 import com.fi.ls.service.LectureService;
 import java.util.List;
@@ -10,8 +11,7 @@ import java.util.NoSuchElementException;
 import java.util.Optional;
 import javax.inject.Inject;
 import javax.transaction.Transactional;
-import org.hibernate.service.spi.ServiceException;
-
+import java.util.Collections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -24,114 +24,124 @@ import org.springframework.stereotype.Service;
 @Transactional
 public class LectureFacadeImpl implements LectureFacade {
 
-	private final Logger logger = LoggerFactory.getLogger(LectureFacadeImpl.class);
+    private final Logger logger = LoggerFactory.getLogger(LectureFacadeImpl.class);
 
-	private LectureService lectureService;
+    private LectureService lectureService;
 
-	private BeanMapping beanMapping;
+    private BeanMapping beanMapping;
 
-	@Inject
-	public LectureFacadeImpl(LectureService lectureService, BeanMapping beanMapping) {
+    @Inject
+    public LectureFacadeImpl(LectureService lectureService, BeanMapping beanMapping) {
 
-		this.lectureService = lectureService;
-		this.beanMapping = beanMapping;
+    	this.lectureService = lectureService;
+	this.beanMapping = beanMapping;
+
+    }
+
+    @Override
+    public Optional<LectureDTO> createLecture(LectureCreateDTO lecture) {
+
+	if (lecture == null) {
+
+            throw new IllegalArgumentException("Param can not be null!");
+
+	}
+	try {
+                        
+            Optional<Lecture> optLect = Optional.of(lectureService.create(beanMapping.mapTo(lecture, Lecture.class).get()));
+            Lecture lect = lectureService.create(optLect.get());
+            return beanMapping.mapTo(lect, LectureDTO.class);
+
+	} catch (NoSuchElementException | ServiceLayerException e) {
+
+            logger.warn("Error occured while creating the LectureDTO!" + e);
+            return Optional.empty();
 
 	}
 
-	@Override
-	public Optional<LectureDTO> createLecture(LectureCreateDTO lecture) {
+    }
 
-		if (lecture == null) {
+    @Override
+    public Optional<LectureDTO> getLectureById(Long id) {
 
-			throw new IllegalArgumentException("Param can not be null!");
+	if (id == null) {
 
-		}
-		Optional<Lecture> optLect = Optional.of(lectureService.create(beanMapping.mapTo(lecture, Lecture.class).get()));
-		try {
+            throw new IllegalArgumentException("Param can not be null!");
 
-			Lecture lect = lectureService.create(optLect.get());
-			return beanMapping.mapTo(lect, LectureDTO.class);
+	}
+	try {
 
-		} catch (NoSuchElementException | ServiceException e) {
+            Lecture lect = lectureService.findById(id);
+            return beanMapping.mapTo(lect, LectureDTO.class);
 
-			logger.warn("Error occured while creating the LectureDTO!" + e);
-			return Optional.empty();
+	} catch (NoSuchElementException | ServiceLayerException e) {
 
-		}
+            logger.warn("Error occured while searching for Lecture!" + e);
+            return Optional.empty();
 
 	}
 
-	@Override
-	public Optional<LectureDTO> getLectureById(Long id) {
+    }
 
-		if (id == null) {
+    @Override
+    public List<LectureDTO> getAllLectures() {
+            
+        try {
+            
+            List<Lecture> lectures = lectureService.findAll();
+            return beanMapping.mapTo(lectures, LectureDTO.class);
+                
+        } catch (NoSuchElementException | ServiceLayerException e) {
+                
+            logger.warn("Error occured while searching for all Lectures!" + e);
+            return Collections.emptyList();
+                
+        }
+		
+    }
 
-			throw new IllegalArgumentException("Param can not be null!");
+    @Override
+    public Optional<LectureDTO> updateLecture(Long id) {
 
-		}
-		try {
+	if (id == null) {
 
-			Lecture lect = lectureService.findById(id);
-			return beanMapping.mapTo(lect, LectureDTO.class);
+            throw new IllegalArgumentException("Param can not be null!");
 
-		} catch (NoSuchElementException | ServiceException e) {
+	}
+	try {
 
-			logger.warn("Error occured while searching for Lecture!" + e);
-			return Optional.empty();
+            Optional<Lecture> optLect = Optional.of(lectureService.update(lectureService.findById(id)));
+            return optLect.isPresent() ? beanMapping.mapTo(optLect.get(), LectureDTO.class) : Optional.empty();
 
-		}
+        } catch (NoSuchElementException | ServiceLayerException e) {
+
+            logger.warn("Error occured while updating the Lecture!" + e);
+            return Optional.empty();
 
 	}
 
-	@Override
-	public List<LectureDTO> getAllLectures() {
+    }
 
-		return beanMapping.mapTo(lectureService.findAll(), LectureDTO.class);
+    @Override
+    public Boolean deleteLecture(Long id) {
+
+        if (id == null) {
+
+            throw new IllegalArgumentException("Param can not be null!");
+
+	}
+	try {
+
+            lectureService.remove(lectureService.findById(id));
+            return true;
+
+	} catch (NoSuchElementException | ServiceLayerException e) {
+
+            logger.warn("Error occured while deleting the Lecture!" + e);
+            return false;
 
 	}
 
-	@Override
-	public Optional<LectureDTO> updateLecture(Long id) {
-
-		if (id == null) {
-
-			throw new IllegalArgumentException("Param can not be null!");
-
-		}
-		try {
-
-			Optional<Lecture> optLect = Optional.of(lectureService.update(lectureService.findById(id)));
-			return optLect.isPresent() ? beanMapping.mapTo(optLect.get(), LectureDTO.class) : Optional.empty();
-
-		} catch (NoSuchElementException | ServiceException e) {
-
-			logger.warn("Error occured while updating the Lecture!" + e);
-			return Optional.empty();
-
-		}
-
-	}
-
-	@Override
-	public Boolean deleteLecture(Long id) {
-
-		if (id == null) {
-
-			throw new IllegalArgumentException("Param can not be null!");
-
-		}
-		try {
-
-			lectureService.remove(lectureService.findById(id));
-			return true;
-
-		} catch (NoSuchElementException | ServiceException e) {
-
-			logger.warn("Error occured while deleting the Lecture!" + e);
-			return false;
-
-		}
-
-	}
+    }
 
 }
